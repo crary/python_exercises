e4628bcc2717a21b3c5f0850527f9e5ed970212a
David Crary <22047656+crary@users.noreply.github.com>
2023-01-19 09:47:51 -0800

    Python Exercises Gently Explained
    Alternate version of exercise #18, buy one get one free.

---
+++ buy1_get1_v2.py
@@ -0,0 +1,14 @@
+numberOfCoffees = input("Enter number of coffees: ")
+pricePerCoffee = input("Enter price per coffee: ")
+
+#Change input to integers
+numberOfCoffees = int(numberOfCoffees)
+pricePerCoffee = int(pricePerCoffee)
+
+def getCostOfCoffee(numberOfCoffees,pricePerCoffee):
+    freeCoffees =  numberOfCoffees // 9
+    totalCost = numberOfCoffees * pricePerCoffee
+    totalCost -= freeCoffees
+    return totalCost
+
+print(getCostOfCoffee(numberOfCoffees,pricePerCoffee))
