895ea3f5f4d46b50af5f68c1d9c4f322c9d3aeae
David Crary <22047656+crary@users.noreply.github.com>
2023-02-17 09:39:00 -0800

    Exercise #24, Every 15 minutes

---
+++ every_15min.py
@@ -0,0 +1,6 @@
+
+
+for meridian in [ 'am', 'pm']:
+    for hour in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+        for minutes in ['00','15', '30', '45']:
+            print( hour + ':' + minutes + ' ' + meridian)
