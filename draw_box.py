19f90fc380936d1426ed1f6129281fc449462c63
David Crary <22047656+crary@users.noreply.github.com>
2023-03-07 11:59:37 -0800

    Drawing lines of box

--- draw_box.py
+++ draw_box.py
@@ -1,6 +1,9 @@
-import border_drawing
+## Exercise 30, draw a box
+#import border_drawing
 
 cubeHeight = 3
-cubeWidth = 3
+cubeWidth = 5
 
+print( "." * (int(cubeWidth) + 1) + "+" + "-"*int(cubeWidth) + "+")
+print( "." * (int(cubeWidth)) + "/" + "-"*int(cubeWidth) + "/|")  
 #border_drawing.drawBorder(cubeHeight,cubeWidth)
