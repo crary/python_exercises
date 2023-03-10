270f9dce5e145740a4ab972c73194a19bd4c5b76
David Crary <22047656+crary@users.noreply.github.com>
2023-03-10 09:34:01 -0800

    ## Exercise 30, draw a box

--- draw_box.py
+++ draw_box.py
@@ -1,17 +1,27 @@
 ## Exercise 30, draw a box
 #import border_drawing
 
-size = 5
 
+size = 3
 
-print( "." * (int(size) + 1) + "+" + "-"*int(size *2) + "+")
-print( "." * (int(size)) + "/" + "-" * (int(size *2) - 0) + "/|")
+## Print top 2x lines of box
+print( "." * (size + 1) + "+" + "-"*(size *2) + "+")
+print( "." * size + "/" + "-" * (size *2) + "/|")
 
+## Print top half of box
 for row in range(size - 1):
     leftSide = "." * ((size - 1) - row)
     middle = "/" + "-" * (int(size *2) - 0) + "/"# + "."*(size + 1)  + "|"
     end = "."*(row + 1)  + "|"
 
-
-
     print(leftSide + middle + end)
+
+## Print middle seam
+print( "+" + "+"*int(size *2) + "+" + "-" * size + "+")
+
+## Print bottom half of box
+for rows in range(size):
+    middle = "+" + "-" * ((size *2) - 0) + "+"
+    rightSide = "." * ((size - 1) - rows) + "/"
+
+    print(middle  + rightSide)
