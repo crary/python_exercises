8f511195d527884aa99a04bda2774f52c450c55f
David Crary <22047656+crary@users.noreply.github.com>
2023-03-09 16:43:27 -0800

    ## Exercise 30, draw a box
    Updated to correctly draw upper portion of box

--- draw_box.py
+++ draw_box.py
@@ -1,9 +1,17 @@
 ## Exercise 30, draw a box
 #import border_drawing
 
-cubeHeight = 3
-cubeWidth = 5
+size = 5
 
-print( "." * (int(cubeWidth) + 1) + "+" + "-"*int(cubeWidth) + "+")
-print( "." * (int(cubeWidth)) + "/" + "-"*int(cubeWidth) + "/|")  
-#border_drawing.drawBorder(cubeHeight,cubeWidth)
+
+print( "." * (int(size) + 1) + "+" + "-"*int(size *2) + "+")
+print( "." * (int(size)) + "/" + "-" * (int(size *2) - 0) + "/|")
+
+for row in range(size - 1):
+    leftSide = "." * ((size - 1) - row)
+    middle = "/" + "-" * (int(size *2) - 0) + "/"# + "."*(size + 1)  + "|"
+    end = "."*(row + 1)  + "|"
+
+
+
+    print(leftSide + middle + end)
