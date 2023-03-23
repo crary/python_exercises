e6880297f42ce9af3866a1d99f57811fdcb8ceed
David Crary <22047656+crary@users.noreply.github.com>
2023-03-23 12:03:39 -0700

    ## Exercise 36, Reverse String

---
+++ reverseString.py
@@ -0,0 +1,17 @@
+
+## Exercise 36, Reverse String
+
+
+def reverseString(text):
+    text = list(text)
+
+    for i in range(len(text) // 2):
+        mirrorIndex = len(text) - 1 - i
+        text[i], text[mirrorIndex] = text[mirrorIndex], text[i]
+
+
+    return ''.join(text)
+
+
+
+print(reverseString("hello"))
